ed485ebff8d28a57568e8a2ea0aa18ca8d742bfd
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2025-01-14 00:12:02 -0300

    Add 100. Same Tree

---
+++ 100. Same Tree.swift
@@ -0,0 +1,38 @@
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+
+        if p == nil && q == nil {
+            return true
+        }
+
+        if p == nil || q == nil {
+            return false
+        }
+
+        if let p, let q {
+            if p.val != q.val {
+                return false
+            }
+        }
+
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+}
